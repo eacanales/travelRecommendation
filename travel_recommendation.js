fcf296c98e336ca8e5d0db1420e6d41c32d9c448
Your Name <you@example.com>
2024-07-15 20:08:02 -0400

    initial commit

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,10 +1,32 @@
-// Ruta al archivo local
-const filePath = `https://eacanales-5500.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/travelRecommendation/travel_recommendation_api.json`;
+document.getElementById('conditionInput').addEventListener('submit',showcountryDetails);
 
-// Carga el archivo local
-fetch(filePath)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        }
-    )
+function showcountryDetails(event) {
+    event.preventDefault();
+
+        const conditionalInput = document.getElementById('conditionInput').value;
+        // Ruta al archivo local
+        const filePath = `https://eacanales-5500.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/travelRecommendation/travel_recommendation_api.json`;
+
+        // Carga el archivo local
+        fetch(filePath)
+            .then(response => response.json())
+            .then(data => {
+                const searchInfo = document.getElementById('searchInfo');
+                if (searchInfo === country){
+                    searchInfo.innerHTML = `<h2>Information requested by searchInfo</h2>
+                                            <img src="sydney-australia.jpeg" alt="Australia" class="australia">`
+
+                } else {
+                alert('try again');
+            }
+    
+    }
+)
+
+
+
+
+
+}
+
+
