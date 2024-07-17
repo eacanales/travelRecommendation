f066912205b6b028908003a4fb322e3321d1018e
Your Name <you@example.com>
2024-07-16 21:24:23 -0400

    initial commit

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,37 +1,40 @@
 const btnSearch = document.getElementById('btnSearch');
 
 function searchRecomm() {
-    const input = document.getElementById('conditionInput').value.toLowerCase();
-    const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = '';
+  const input = document.getElementById('conditionInput').value.toLowerCase();
+  const resultDiv = document.getElementById('result');
+  resultDiv.innerHTML = '';
 
-        // Ruta al archivo local
-        const filePath = `https://eacanales-5500.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/travelRecommendation/travel_recommendation_api.json`;
-        fetch(filePath)
-            .then(response => response.json())
-            .then(data => {
-                const country = data.countries.find(item => item.name.toLowerCase() === input);
-                /*const city = data.countries.cities.name;*/
+  // Ruta al archivo local
+  const filePath = `https://eacanales-5500.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/travelRecommendation/travel_recommendation_api.json`;
+  fetch(filePath)
+    .then(response => response.json())
+    .then(data => {
+      const country = data.countries.find(item => item.name.toLowerCase() === input);
 
-                if (country) {
-                    
-
-                    resultDiv.innerHTML += `<h2>${country.name}</h2>`;
-                    /*resultDiv.innerHTML += `<h2>${city.name}</h2>`;*/
-                    resultDiv.innerHTML += `<img src="${country.imageUrl}" alt="${country.name}">`;
-                    resultDiv.innerHTML += `<p><strong>TEST: </strong> test </p>`;
-
-                } else {
-                  resultDiv.innerHTML = 'Condition not found.';
-                }
-    
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                resultDiv.innerHTML = 'An error ocurred while fetching data';
-            });
-
+      if (country) {
+        resultDiv.innerHTML += `<h2>${country.name}</h2>`;
+        
+        // Iterar sobre las ciudades de Australia y mostrarlas
+        country.cities.forEach(city => {
+          resultDiv.innerHTML += `
+            <div>
+              <h3>${city.name}</h3>
+              <img src="${city.imageUrl}" alt="${city.name}">
+              <p>${city.description}</p>
+            </div>
+          `;
+        });
+      } else {
+        resultDiv.innerHTML = 'Country not found.';
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      resultDiv.innerHTML = 'An error occurred while fetching data';
+    });
 }
 
 btnSearch.addEventListener('click', searchRecomm);
+
 
