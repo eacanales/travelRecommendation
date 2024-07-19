63fc5975b37b15d4aac09375e643a5fb45e5f51d
Your Name <you@example.com>
2024-07-19 17:42:34 -0400

    initial commit

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -8,13 +8,26 @@ function searchRecomm() {
   const resultDiv = document.getElementById('result');
   resultDiv.innerHTML = '';
   
-  // Ruta al archivo local
   const filePath = `travel_recommendation_api.json`;
   fetch(filePath)
     .then(response => response.json())
     .then(data => {
-      const country = data.countries.find(item => item.name.toLowerCase() === input);
+        if (input === "country") {
+            data.countries.forEach(country => {
+              resultDiv.innerHTML += `<h2>${country.name}</h2>`;
+              country.cities.forEach(city => {
+                resultDiv.innerHTML += `
+                  <div>
+                    <h3>${city.name}</h3>
+                    <img src="${city.imageUrl}" alt="${city.name}">
+                    <p>${city.description}</p>
+                  </div>
+                `;
+              });
+            });
 
+      /*const country = data.countries.find(item => item.name.toLowerCase() === input);
+      alert(JSON.stringify(country)); 
       if (country) {
         resultDiv.innerHTML += `<h2>${country.name}</h2>`;
         
@@ -27,7 +40,8 @@ function searchRecomm() {
               <p>${city.description}</p>
             </div>
           `;
-        });
+        });*/
+
       } else {
         resultDiv.innerHTML = 'Country not found.';
       }
@@ -36,7 +50,7 @@ function searchRecomm() {
       console.error('Error:', error);
       resultDiv.innerHTML = 'An error occurred while fetching data';
     });
-}
+} 
 
 btnSearch.addEventListener('click', searchRecomm);
 
